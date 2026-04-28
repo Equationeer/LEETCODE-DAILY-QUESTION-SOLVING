f72a84436321b6522a2e61c3e926a255900f5b81
Alokkumar Gupta <alokkumar568g@gmail.com>
2026-04-28 19:38:04 +0530

    Update 4Sum.cpp

--- 18. 4Sum/4Sum.cpp
+++ 18. 4Sum/4Sum.cpp
@@ -8,10 +8,12 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue; 
+            if (i > 0 && nums[i] == nums[i - 1]) 
+                continue; 
 
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue; j
+                if (j > i + 1 && nums[j] == nums[j - 1]) 
+                    continue; j
 
                 long long newTarget = (long long)target - nums[i] - nums[j];
                 int l = j + 1, r = n - 1;
